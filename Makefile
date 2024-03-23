62da450d5654716adac0f0d60f7960265cc00423
Alfis Salam <alfis.salam1@gmail.com>
2024-03-23 09:55:47 +0700

    first commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+swag:
+	swag init -g cmd/main.go -o cmd/docs
+cover:
+	go test -cover ./...
---
+++ internal/model/user.go
@@ -0,0 +1,56 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+
+	Email     string         `json:"email"`
+	Password  string         `json:"-"`
+	DoB       time.Time      `json:"dob" gorm:"column:dob"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"column:deleted_at"`
+}
+
+type DefaultColumn struct {
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at"`
+}
+
+type UserMediaSocial struct {
+	ID        uint64 `json:"id"`
+	UserID    uint64 `json:"user_id"`
+	Title     string `json:"title"`
+	Url       string `json:"url"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time `json:"deleted_at,omitempty"`
+}
+
+// https://blog.logrocket.com/gin-binding-in-go-a-tutorial-with-examples/
+// https://gin-gonic.com/docs/examples/binding-and-validation/
+type UserSignUp struct {
+	Username string `json:"username" binding:"required"`
+	Password string    `json:"password" binding:"required"`
+	Email    string    `json:"email"`
+	DoB      time.Time `json:"dob"`
+}
+
+func (u UserSignUp) Validate() error {
+	// check username
+	if u.Username == "" {
+		return errors.New("invalid username")
+	}
+	if len(u.Password) < 6 {
+		return errors.New("invalid password")
+	}
+	return nil
+}
